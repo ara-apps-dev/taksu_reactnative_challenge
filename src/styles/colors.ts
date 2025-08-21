f6d71acb3ef796123a71d7e46e79ad8b15e62545
guska <ipg.guska.p@gmail.com>
2025-08-22 03:12:27 +0800

    feat: add colors, update fonts and global styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,39 @@
+export const Colors = {
+  // Status
+  statusOpen: '#999999',
+  statusDone: '#00C853',
+  statusOverdue: '#D50000',
+
+  // Icon
+  iconWhite: '#FFFFFF',
+
+  // Text
+  textPrimary: '#414141',
+  textWhite: '#FFFFFF',
+  textError: '#FF5252',
+
+  // Backgrounds
+  bgPrimary: '#36393F',
+  bgSecondary: '#E0E0E0',
+  bgAccent: '#FFEB3B',
+
+  // Buttons Variants
+  buttonVariantPrimary: '#5440D1',
+  buttonVariantSuccess: '#39C36D',
+
+  // Buttons
+  buttonPrimary: '#6200EE',
+  buttonSecondary: '#03DAC6',
+
+  // Borders
+  borderDefault: '#DDDDDD',
+
+  // Card
+  cardPrimary: '#40444B',
+
+  // Chip
+  chipPrimary: '#C4C4C4',
+
+  // Transparent
+  transparent: 'transparent',
+};
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,4 +14,5 @@ export const globalStyles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  errorText: { color: 'red' },
 });
